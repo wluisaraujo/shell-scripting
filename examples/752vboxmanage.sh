27fb32f4b1d3479b5a2c5532ca6672c278d06042
W. Luís Araújo <w.luis.araujo@gmail.com>
2019-08-09 10:18:17 -0300

    Update 752vboxmanage.sh

--- examples/752vboxmanage.sh
+++ examples/752vboxmanage.sh
@@ -8,16 +8,10 @@ for VM in $OVAFILE
   do  vboxmanage import APPDIR/$VM
 done
 
-# criar rede interna
-vboxmanage create hostonlyif
+# criar NIC rede interna
+#vboxmanage create hostonlyif
 
 # associar rede nat e interna
-
-
-VBoxManage modifyvm vmtest --ostype  "Linux26_64" --memory 4096 --acpi on --cpus 1  --description "VM Test"
-
-
-VBoxManage modifyvm OracleLinux6Test --nic1 bridged --bridgeadapter1 eth0
-
-
-VBoxManage modifyvm <vmname> --intnet<X> <network_name>.
+#VBoxManage modifyvm vmtest --ostype  "Linux26_64" --memory 4096 --acpi on --cpus 1  --description "VM Test"
+#VBoxManage modifyvm OracleLinux6Test --nic1 bridged --bridgeadapter1 eth0
+#VBoxManage modifyvm <vmname> --intnet<X> <network_name>.
