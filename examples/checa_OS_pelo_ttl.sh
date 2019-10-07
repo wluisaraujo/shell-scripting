e932576c95b77d0b9dafe3e1ffb8fdae7e325081
W. Luís Araújo <w.luis.araujo@gmail.com>
2019-10-07 13:32:19 -0300

    Update checa_OS_pelo_ttl.sh

--- examples/checa_OS_pelo_ttl.sh
+++ examples/checa_OS_pelo_ttl.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-# 64 bytes from 172.28.126.46: icmp_seq=2 ttl=64 time=0.109 ms
 
-TTL=$(ping -c3  $1  | grep -i ttl | tail -n1 | awk '{print $6}')
+TTL=$(ping -c3  $1  | grep ttl| tail -n1 | awk '{ print $6 }')
 
-echo "O $TTL"
-if [ $TTL = 'ttl=64' ] 
+echo "O TTL de $1 é $TTL"
+if [ $TTL = "ttl=64" ] 
   then
-	echo "O sistem é linux com  $TTL"
-		else
-	echo "O sistem é windows com  $TTL"
+    echo "O host $1 é tem o $TTL é um sistema linux"
+  else
+    echo "O host $1 é tem o $TTL é um sistema Windows"
 fi
